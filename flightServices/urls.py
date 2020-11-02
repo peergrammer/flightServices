bb2887ee51fbb061e726f32f6bd5538e52b7be60
peergrammer <phdwight@gmail.com>
2020-11-02 10:17:16 +0800

    Initial load of database
    initial migrations

--- flightServices/urls.py
+++ flightServices/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from flightApp import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('flights', views.FlightViewSet)
+router.register('reservations', views.ReservationViewSet)
+router.register('passengers', views.PassengerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('flight-services/', include(router.urls))
 ]
